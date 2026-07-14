c111a2ed44d39fac4c1200f315e137a67e48f6e4
Elias <elias.lanvin@roojai.com>
2026-07-14 12:44:51 +0700

    feat: add semantic typography Mix tokens

---
+++ lib/src/tokens/semantic/typography.dart
@@ -0,0 +1,21 @@
+// lib/src/tokens/semantic/typography.dart
+
+import 'package:mix/mix.dart';
+
+const $displayMd = TextStyleToken('typography.displayMd');
+const $displaySm = TextStyleToken('typography.displaySm');
+const $h1 = TextStyleToken('typography.h1');
+const $h2 = TextStyleToken('typography.h2');
+const $h3 = TextStyleToken('typography.h3');
+const $h4 = TextStyleToken('typography.h4');
+const $bodyLg = TextStyleToken('typography.bodyLg');
+const $bodyMd = TextStyleToken('typography.bodyMd');
+const $bodySm = TextStyleToken('typography.bodySm');
+const $labelLg = TextStyleToken('typography.labelLg');
+const $labelMd = TextStyleToken('typography.labelMd');
+const $labelSm = TextStyleToken('typography.labelSm');
+const $captionMd = TextStyleToken('typography.captionMd');
+const $captionSm = TextStyleToken('typography.captionSm');
+const $overline = TextStyleToken('typography.overline');
+const $small = TextStyleToken('typography.small');
+const $footnote = TextStyleToken('typography.footnote');
