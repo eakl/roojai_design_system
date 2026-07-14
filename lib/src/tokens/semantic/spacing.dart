d53c648c88e49d4867d95c3f2927101491c10495
Elias <elias.lanvin@roojai.com>
2026-07-14 12:51:12 +0700

    feat: add semantic spacing Mix tokens

---
+++ lib/src/tokens/semantic/spacing.dart
@@ -0,0 +1,19 @@
+// lib/src/tokens/semantic/spacing.dart
+
+import 'package:mix/mix.dart';
+
+const $spacing0 = SpaceToken('spacing.0');
+const $spacing2 = SpaceToken('spacing.2');
+const $spacing4 = SpaceToken('spacing.4');
+const $spacing6 = SpaceToken('spacing.6');
+const $spacing8 = SpaceToken('spacing.8');
+const $spacing12 = SpaceToken('spacing.12');
+const $spacing16 = SpaceToken('spacing.16');
+const $spacing20 = SpaceToken('spacing.20');
+const $spacing24 = SpaceToken('spacing.24');
+const $spacing32 = SpaceToken('spacing.32');
+const $spacing40 = SpaceToken('spacing.40');
+const $spacing48 = SpaceToken('spacing.48');
+const $spacing64 = SpaceToken('spacing.64');
+const $spacing80 = SpaceToken('spacing.80');
+const $spacing96 = SpaceToken('spacing.96');
