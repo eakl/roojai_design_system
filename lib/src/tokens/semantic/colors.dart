2b033e6e2867a19df70f51b574c9a7f5abb6a730
Elias <elias.lanvin@roojai.com>
2026-07-14 12:43:05 +0700

    feat: add semantic color Mix tokens

---
+++ lib/src/tokens/semantic/colors.dart
@@ -0,0 +1,69 @@
+// lib/src/tokens/semantic/colors.dart
+
+import 'package:mix/mix.dart';
+
+// Canvas.
+const $canvasBase = ColorToken('color.canvas.base');
+const $canvasAlternative = ColorToken('color.canvas.alternative');
+
+// Surface.
+const $surfaceBase = ColorToken('color.surface.base');
+const $surfaceAlternative = ColorToken('color.surface.alternative');
+const $surfaceInverted = ColorToken('color.surface.inverted');
+
+// Content.
+const $contentPrimary = ColorToken('color.content.primary');
+const $contentSecondary = ColorToken('color.content.secondary');
+const $contentMuted = ColorToken('color.content.muted');
+const $contentPlaceholder = ColorToken('color.content.placeholder');
+const $contentOnBrand = ColorToken('color.content.onBrand');
+const $contentOnBrandMuted = ColorToken('color.content.onBrandMuted');
+
+// Border.
+const $borderBase = ColorToken('color.border.base');
+const $borderStrong = ColorToken('color.border.strong');
+
+// Positive.
+const $positiveSurface = ColorToken('color.positive.surface');
+const $positiveSurfaceStrong = ColorToken('color.positive.surfaceStrong');
+const $positiveBorder = ColorToken('color.positive.border');
+const $positiveUi = ColorToken('color.positive.ui');
+const $positiveUiHover = ColorToken('color.positive.uiHover');
+const $positiveText = ColorToken('color.positive.text');
+const $positiveTextStrong = ColorToken('color.positive.textStrong');
+
+// Negative.
+const $negativeSurface = ColorToken('color.negative.surface');
+const $negativeSurfaceStrong = ColorToken('color.negative.surfaceStrong');
+const $negativeBorder = ColorToken('color.negative.border');
+const $negativeUi = ColorToken('color.negative.ui');
+const $negativeUiHover = ColorToken('color.negative.uiHover');
+const $negativeText = ColorToken('color.negative.text');
+const $negativeTextStrong = ColorToken('color.negative.textStrong');
+
+// Warning.
+const $warningSurface = ColorToken('color.warning.surface');
+const $warningSurfaceStrong = ColorToken('color.warning.surfaceStrong');
+const $warningBorder = ColorToken('color.warning.border');
+const $warningUi = ColorToken('color.warning.ui');
+const $warningUiHover = ColorToken('color.warning.uiHover');
+const $warningText = ColorToken('color.warning.text');
+const $warningTextStrong = ColorToken('color.warning.textStrong');
+
+// Alert.
+const $alertSurface = ColorToken('color.alert.surface');
+const $alertSurfaceStrong = ColorToken('color.alert.surfaceStrong');
+const $alertBorder = ColorToken('color.alert.border');
+const $alertUi = ColorToken('color.alert.ui');
+const $alertUiHover = ColorToken('color.alert.uiHover');
+const $alertText = ColorToken('color.alert.text');
+const $alertTextStrong = ColorToken('color.alert.textStrong');
+
+// Info.
+const $infoSurface = ColorToken('color.info.surface');
+const $infoSurfaceStrong = ColorToken('color.info.surfaceStrong');
+const $infoBorder = ColorToken('color.info.border');
+const $infoUi = ColorToken('color.info.ui');
+const $infoUiHover = ColorToken('color.info.uiHover');
+const $infoText = ColorToken('color.info.text');
+const $infoTextStrong = ColorToken('color.info.textStrong');
